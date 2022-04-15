c126ad6f07a0eba3049f902937668730df6710c8
ManuLG <ManuLG@users.noreply.github.com>
2022-04-15 15:39:14 +0000

    deploy: f808065cb4fbcf49dfb31b05e93bd07293e18459

---
+++ _nuxt/74bb0ee.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{233:function(n,e,t){"use strict";var l=t(9).default.extend({name:"default"}),c=t(78),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("nuxt")}),[],!1,null,null,null);e.a=component.exports},235:function(n,e,t){t(236),n.exports=t(237)}},[[235,5,1,6]]]);
