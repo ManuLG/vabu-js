338157d5d252de481a3558c3c242765b0bb49470
ManuLG <ManuLG@users.noreply.github.com>
2022-04-15 16:21:06 +0000

    deploy: 43066f3f23fdee131943cbe4bfb69ad7504b0290

---
+++ _nuxt/d068243.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{233:function(n,e,t){"use strict";var l=t(10).default.extend({name:"default"}),c=t(67),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("nuxt")}),[],!1,null,null,null);e.a=component.exports},235:function(n,e,t){t(236),n.exports=t(237)}},[[235,6,1,7]]]);
---
+++ _nuxt/static/1650039664/demo/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fvabu-js\u002F_nuxt\u002Fstatic\u002F1650039664",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdemo",config:{_app:{basePath:"\u002Fvabu-js\u002F",assetsPath:"\u002Fvabu-js\u002F_nuxt\u002F",cdnURL:a}}}}(null));
